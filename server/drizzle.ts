76d37625dfb77431c9a687eab58bc6da16bb0952
Josias Aurel <ndjosiasaurel@gmail.com>
2025-07-22 13:38:22 +0100

    include schemas needed for authentication in drizzle

--- server/drizzle.ts
+++ server/drizzle.ts
@@ -1,24 +1,88 @@
 import { config } from "dotenv";
 import { drizzle } from "drizzle-orm/libsql";
-import { int, text, sqliteTable } from "drizzle-orm/sqlite-core";
+import { int, integer, text, sqliteTable } from "drizzle-orm/sqlite-core";
 
 // load environment variables
 config();
 
-export const accountsTable = sqliteTable("accounts_table", {
-    id: int().primaryKey({ autoIncrement: true }),
-    email: text().notNull(),
-    username: text().notNull(),
-    streakCount: int().notNull(),
-    timezone: int().notNull(),
-});
+// export const accountsTable = sqliteTable("accounts_table", {
+//     id: int().primaryKey({ autoIncrement: true }),
+//     email: text().notNull(),
+//     username: text().notNull(),
+//     streakCount: int().notNull(),
+//     timezone: int().notNull(),
+// });
 
-export const updatesTable = sqliteTable("updates_table", {
+export const updates = sqliteTable("updates_table", {
     id: int().primaryKey({ autoIncrement: true }),
     postTime: int().notNull(), // timestamp
     text: text().notNull(),
     attachments: text(),
     source: text().notNull() // string representing the client which sent the update
+});
+
+export const users = sqliteTable("users", {
+  id: text("id").primaryKey(),
+  name: text("name").notNull(),
+  email: text("email").notNull().unique(),
+  emailVerified: integer("email_verified", { mode: "boolean" })
+    .$defaultFn(() => false)
+    .notNull(),
+  image: text("image"),
+  createdAt: integer("created_at", { mode: "timestamp" })
+    .$defaultFn(() => /* @__PURE__ */ new Date())
+    .notNull(),
+  updatedAt: integer("updated_at", { mode: "timestamp" })
+    .$defaultFn(() => /* @__PURE__ */ new Date())
+    .notNull(),
+});
+
+export const sessions = sqliteTable("sessions", {
+  id: text("id").primaryKey(),
+  expiresAt: integer("expires_at", { mode: "timestamp" }).notNull(),
+  token: text("token").notNull().unique(),
+  createdAt: integer("created_at", { mode: "timestamp" }).notNull(),
+  updatedAt: integer("updated_at", { mode: "timestamp" }).notNull(),
+  ipAddress: text("ip_address"),
+  userAgent: text("user_agent"),
+  userId: text("user_id")
+    .notNull()
+    .references(() => users.id, { onDelete: "cascade" }),
+});
+
+export const accounts = sqliteTable("accounts", {
+  id: text("id").primaryKey(),
+  accountId: text("account_id").notNull(),
+  providerId: text("provider_id").notNull(),
+  userId: text("user_id")
+    .notNull()
+    .references(() => users.id, { onDelete: "cascade" }),
+  accessToken: text("access_token"),
+  refreshToken: text("refresh_token"),
+  idToken: text("id_token"),
+  accessTokenExpiresAt: integer("access_token_expires_at", {
+    mode: "timestamp",
+  }),
+  refreshTokenExpiresAt: integer("refresh_token_expires_at", {
+    mode: "timestamp",
+  }),
+  scope: text("scope"),
+  password: text("password"),
+  createdAt: integer("created_at", { mode: "timestamp" }).notNull(),
+  updatedAt: integer("updated_at", { mode: "timestamp" }).notNull(),
+});
+
+export const verifications = sqliteTable("verifications", {
+  id: text("id").primaryKey(),
+  identifier: text("identifier").notNull(),
+  value: text("value").notNull(),
+  expiresAt: integer("expires_at", { mode: "timestamp" }).notNull(),
+  createdAt: integer("created_at", { mode: "timestamp" }).$defaultFn(
+    () => /* @__PURE__ */ new Date(),
+  ),
+  updatedAt: integer("updated_at", { mode: "timestamp" }).$defaultFn(
+    () => /* @__PURE__ */ new Date(),
+  ),
 });
 
 export const db = drizzle(process.env.DB_FILE_NAME!);
